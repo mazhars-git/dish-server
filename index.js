1c9fa9fb48ac9e83f4c4f20e769ab8a9f341a870
Md. Mazharul Islam <mazharulislam611@gmail.com>
2022-10-17 22:47:55 +0600

    add item

--- index.js
+++ index.js
@@ -35,6 +35,12 @@ async function run(){
       const query = {_id: ObjectId(id)}
       const item = await itemCollection.findOne(query);
       res.send(item);
+    });
+
+    app.post('/item', async(req, res) =>{
+      const newItem = req.body;
+      const result = await itemCollection.insertOne(newItem);
+      res.send(result);
     })
 
 
